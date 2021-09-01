d912dfb3a3678e78aab9cb852def91ca94fd8a9f
dearxuanres <89637526+dearxuanres@users.noreply.github.com>
2021-09-01 17:37:39 +0800

    Add files via upload

---
+++ aplayer/music.js
@@ -0,0 +1,19 @@
+var message_play1 = ['开始播放','','我最喜欢听','哇,','要准备听','你喜欢听'];
+var message_play2 = ['啦~','超级好听!!!','啦~','好好听呀~','了哦~','吗?'];
+var message_pause = 
+['人家还想接着听呢~',
+'是这首歌不好听吗?',
+'要不换一首歌吧~',
+'你怎么暂停了呀!!!',
+'人家还想听呢~']
+var message_seeked = 
+['你好像有点迫不及待了呢……',
+'着急着快进吗?',
+'为什么不认真听完呢?',
+'要不直接听高潮吧!',
+'是这首歌不好听吗?'];
+var message_volume = 
+['这样的声音大小合适吗?',
+'选择一个适合你的音量吧~'];
+
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('o(d.n().m(r)<=-1){q.d.g=h}2 l(0){c i=3.6(3.5()*9.7);c 8=0.b.j[0.b.z].8;4(9[i]+8+B[i])}2 x(0){4(a[3.6(3.5()*a.7)])}2 s(0){}2 u(0){}2 w(0){4(\'v,A!!!\')}2 t(0){4(e[3.6(3.5()*e.7)])}2 y(0){4(f[3.6(3.5()*f.7)])}2 k(0){}2 p(0){}',38,38,'player||function|Math|say|random|floor|length|title|message_play1|message_pause|list|var|location|message_seeked|message_volume|href|mysite||audios|player_onProgress|player_onPlay|indexOf|toString|if|player_onListSwitch|document|checksite|player_onEnded|player_onSeeked|player_onWaiting|糟糕|player_onError|player_onPause|player_onVolumechange|index|出错了|message_play2'.split('|'),0,{}))
